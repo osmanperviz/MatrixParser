32dd4fd6e9f3574d6191e9df167771ce2989bf5e
Osman Perviz <osman.perviz@evopark.de>
2017-04-09 00:24:58 +0200

    Add spec for fetch_and_unzip_service

--- spec/services/fetch_and_unzip_service.rb
+++ spec/services/fetch_and_unzip_service.rb
@@ -1,19 +1,28 @@
 RSpec.describe FetchAndUnzipService do
-  subject { described_class }
-    # let(:files) do
-    #   Rails.application.config.source_names.map do |entry|
-    #     "#{entry}.zip"
-    #   end.sort
-    # end
+subject { described_class }
+  let(:zip_files) do
+    %w(sentinels sniffers loopholes).map do |name|
+      "#{name}.zip"
+    end.sort
+  end
   describe '#call' do
-    context 'files' do
-      it 'has all required files' do
+    context '#fetch_files' do
+      it 'has success response' do
         VCR.use_cassette('first_fetch') do
           response = subject.call
           expect(response).to be_success
         end
       end
+      it 'create zip files in temp folder' do
+        expect((Dir.entries(Rails.root.join('tmp')) & zip_files).sort).to eq(zip_files)
+      end
     end
-
+    context '#unzip' do
+      it 'unzips ziped files' do
+        %w(sentinels sniffers loopholes).each do |src|
+          expect(Dir.entries(Rails.root.join('tmp', src)).count).to be > 0
+        end
+      end
+    end
   end
 end
