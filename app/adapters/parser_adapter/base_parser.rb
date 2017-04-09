ddeb1cd6c31066d3cdeb3a7d3631947574acf680
Osman Perviz <osman.perviz@evopark.de>
2017-04-09 21:05:40 +0200

    Add starting point to parser base class.

--- app/adapters/parser_adapter/base_parser.rb
+++ app/adapters/parser_adapter/base_parser.rb
@@ -1,4 +1,10 @@
 module ParserAdapter
   class BaseParser
+    attr_reader :file_name
+
+    def initialize(file_name)
+      @file_name = file_name
+      @files = Dir.glob(Rails.root.join('tmp', file_name, '*')).entries
+    end
   end
 end
